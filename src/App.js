b23a620487f6571db1b5b0f3b06f402229ef135a
e.langelaar <e.langelaar@tedas.nl>
2021-02-03 20:48:53 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import styled from "styled-components";
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+import Menu from "./components/MenuComponent";
+import Routing from "./components/RoutingComponent";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#c4e4cc",
+    },
+    secondary: {
+      main: "#303C32",
+    }
+  },
+  typography: {
+    allVariants: {
+      color: "#303C32"
+    }
+  }
+});
+
+const StyledWrapper = styled.div`
+  margin-top: 64px;
+  padding: 1em;
+`;
+
+export default() => {
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Menu title="FoodPickr" />
+        <StyledWrapper>
+          <Routing />
+        </StyledWrapper>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
